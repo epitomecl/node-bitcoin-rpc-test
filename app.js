a88c6bdb4b6ad48b7c4f5297a0892efff5be59f0
elegant651 <korea651@gmail.com>
2018-04-16 16:59:46 +0900

    modified server routing

--- app.js
+++ app.js
@@ -8,6 +8,24 @@ app.use(bodyParser.json())
 app.use((err, req, res, next) => next());
 
 app.use('/web', express.static(__dirname + '/client'));
+app.get('/web/*', (req, res) => {
+	const options = {
+		root: __dirname + '/client',
+		dotfiles: 'deny',
+		headers: {
+			'x-timestamp': Date.now(),
+			'x-sent': true
+		}
+		};
+	res.sendFile("index.html", options, function(err) {
+		if (err) {
+			console.log(err);
+			res.status(err.status).end();
+		} else {
+			console.log('client web');
+		}
+	});
+});
 
 const key = JSON.parse(fs.readFileSync('./.key', 'utf8'));
 require('./rpc')(app, key);
